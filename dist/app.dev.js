090181a86e35b8ddab691d090942b04bc1d2bae9
bambie669 <raduroman94@gmail.com>
2020-03-09 03:10:53 +0200

    added controllers

---
+++ dist/app.dev.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var path = require('path');
+
+var express = require('express');
+
+var bodyParser = require('body-parser');
+
+var errorController = require('./controllers/error');
+
+var app = express();
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+var adminRoutes = require('./routes/admin');
+
+var shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(express["static"](path.join(__dirname, 'public')));
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(errorController.get404);
+app.listen(4000);
