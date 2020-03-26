16423c65c1dcab6187fdb6b909bdabdaf87404e7
bambie669 <raduroman94@gmail.com>
2020-03-26 19:30:37 +0200

    bugfix for hashedPass

--- controllers/auth.js
+++ controllers/auth.js
@@ -32,29 +32,34 @@ exports.postLogin = (req, res, next) => {
 };
 
 exports.postSignup = (req, res, next) => {
-  const email =  req.body.email;
-  const password =  req.body.password;
-  const confirmPassword =  req.body.confirmPassword;
-  User.findOne({email: email})
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({
+      email: email
+    })
     .then(userDoc => {
       if (userDoc) {
         return res.redirect('/signup');
       }
-      return bcrypt.hash(password, 12);
+      return bcrypt
+        .hash(password, 12)
+        .then(hashedPassword => {
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: {
+              items: []
+            }
+          });
+          return user.save();
+        })
+        .then(result => {
+          res.redirect('/login')
+        });
     })
-    .then(hashedPassword => {
-      const user = new User({
-        email: email,
-        password: hashedPassword,
-        cart: { items: [] }
-      });
-      return user.save();
-    })  
-    .then(result => {
-      res.redirect('/login')
-    })
-      .catch(err => {
-        console.log(err);
+    .catch(err => {
+      console.log(err);
     });
 };
 
